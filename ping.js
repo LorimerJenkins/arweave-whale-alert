8757dca1272d4801d2b241f98c3d9a045d0c1bc5
Tate Berenbaum <tateflyer1@gmail.com>
2023-07-05 16:24:50 -0400

    fix: twitter API integration

--- ping.js
+++ ping.js
@@ -39,13 +39,15 @@ async function sendTweet(message) {
     const twitterClient = new TwitterApi({
         appKey: process.env.TWITTER_API_KEY,
         appSecret: process.env.TWITTER_API_SECRET,
-        // Following access tokens are not required if you are
-        // at part 1 of user-auth process (ask for a request token)
-        // or if you want a app-only client (see below)
         accessToken: process.env.TWITTER_ACCESS_TOKEN,
-        accessSecret: process.env.TWITTER_ACCESS_SECRET,      
-    });
-    let res = await twitterClient.v1.tweet(message);
+        accessSecret: process.env.TWITTER_ACCESS_SECRET,       
+    }, );
+    let res;
+    try {
+        res = await twitterClient.v2.tweet(message);
+    } catch (err) {
+        console.log(err);
+    }
     console.log(res);
 }
 
